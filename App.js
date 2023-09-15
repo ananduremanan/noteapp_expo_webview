5562efff4df3f0446dabecb08793396189ba9b8a
Anandhu Remanan <48993261+ananduremanan@users.noreply.github.com>
2023-09-15 14:49:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+import { SafeAreaView, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <WebView
+        source={{ uri: 'https://nextnoteapp.netlify.app/' }}
+        // style={{ flex: 1 }}
+      />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#3b82f6',
+    // padding: 8,
+    paddingTop: 40
+  },
+});
